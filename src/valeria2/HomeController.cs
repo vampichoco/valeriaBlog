06e9b389b51597895373a5d2ce9e05008c9f68a3
vampichoco <eskimo@sadzee.com>
2016-10-28 19:32:00 -0500

    toy
    I got bored of javascript, node.js and all the trendy frameworks, so i
    decided to try something different and now i'm playing with .NET Core
    and ASP.NET Core.

---
+++ src/valeria2/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace valeria2
+{
+    public class HomeController
+    {
+        // GET: /<controller>/
+
+        public static IMongoClient _client;
+        public static IMongoDatabase _dataBase;
+
+        public static async Task hello(HttpContext ctx)
+        {
+            //string name;
+            //if (ctx.Request.Query.ContainsKey("name") == false)
+            //{
+            //    name = "World";
+            //}else
+            //{
+            //    name = ctx.Request.Query["name"];
+            //}
+            
+            //await ctx.Response.WriteAsync(string.Format("hello, {0}", name));
+
+            _client = new MongoClient();
+            _dataBase = _client.GetDatabase("test");
+
+            var collection = _dataBase.GetCollection<BsonDocument>("vdb");
+            var filter = new BsonDocument();
+
+            var result = await collection.Find(filter).ToListAsync(); 
+
+            foreach (BsonDocument item in result)
+            {
+                string op = string.Format("<strong>title: {0}</strong><br /> Text: {1}<br />", item.GetValue("title").ToString(), item.GetValue("text").ToString());
+                await ctx.Response.WriteAsync(op);
+            }
+            
+
+
+        }
+    }
+}
---
+++ src/valeria2/post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace valeria2
+{
+    public class post
+    {
+        private string _title;
+        private string _text; 
+
+        public string title
+        {
+            get { return _title; }
+            set { _title = value; }
+        } 
+
+        public string text
+        {
+            get { return text; }
+            set { _text = value; }
+        }
+
+    }
+}
